1ba2d9f85b3452d9e9ac40ac9f4f1d65546a53e1
Nithin Peter <nithinpeter7@gmail.com>
2017-07-11 13:52:38 +1000

    Add source matching and replace logic.

--- examples/app.js
+++ examples/app.js
@@ -1,1 +1,12 @@
-console.log('hellooooo');
+/*start:story:1*/
+const Button = () => {
+  console.log('this is button code');
+};
+/*end:story:1*/
+
+/*start:story:2*/
+const Button2 = () => {
+  console.log('this is button code');
+};
+/*end:story:2*/
+const stories = createStories('Button').add('Simple', Button, `/*story:1*/ `);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ const loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
   const options = loaderUtils.getOptions(this);
-  this.callback(null, result);
+  const regex = /\/\*start:story:(.+?)\*\/\n([\s\S]*?)\/\*end:story:(.+?)\*\//gm;
+
+  while ((result = regex.exec(source)) !== null) {
+    const storyId = result[1];
+    const matchStr = result[2];
+
+    const replaceRegex = new RegExp(
+      `\/[*]{1}story:` + storyId + `[*]{1}\/`,
+      'g'
+    );
+    source = source.replace(replaceRegex, matchStr);
+  }
+
+  this.callback(null, source);
   return;
 };
+
+// TODO
+
+// type options = {
+//   multiple: boolean,
+//   pattern: string,
+// };
+
+// matchStartPattern: /*start:[pattern]:[id]*/
+// matchEndPattern: /*end:[pattern]:[id]*/
+
+// replacePattern: /*[pattern]:[id]*/
